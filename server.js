67697b8b4c83b5e0b1fc7348e010ae4c6c1728b9
SyedSabir895 <sabirsyed2005@gmail.com>
2025-12-29 13:41:09 +0530

    c

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const helmet = require('helmet');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://codekaro-frontend.onrender.com"], credentials: true }));
 app.use(express.json()); // Preferred over bodyParser.json()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet({
